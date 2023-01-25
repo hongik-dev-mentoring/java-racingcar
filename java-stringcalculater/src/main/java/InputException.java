cc1092fb0527819b719fe4c2fedbe13a0eb18b93
LTSGOD <lts0424@naver.com>
2023-01-25 21:00:57 +0900

    feat(InputException): 예외 처리 기능 추가
    - getNumOrThrow : 숫자면 숫자 반환 숫자가 아니면 예외 발생
    - ifNegativeThenThrow : 음수가 들어오면 예외 발생

--- java-stringcalculater/src/main/java/InputException.java
+++ java-stringcalculater/src/main/java/InputException.java
@@ -1,6 +1,16 @@
-public class Exception {
+public class InputException {
 
-	public static void handleNull(String input){
-		if(input == null) throw 
+	public static int getNumOrThrow(String token) {
+		int num;
+		try{
+			num = Integer.parseInt(token);
+		}catch(NumberFormatException e){
+			throw new RuntimeException("숫자를 입력하세요.");
+		}
+		return num;
+	}
+
+	public static void ifNegativeThenThrow(int num) {
+		if(num < 0) throw new RuntimeException("양수를 입력하세요.");
 	}
 }

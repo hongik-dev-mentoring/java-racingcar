32600733717724848b35ea00018bc57ceec62c2d
Junek3 <rlarudrms28@gmail.com>
2023-01-27 08:02:58 +0900

    feat: Car 이동하기, position 출력하기 구현
    난수 생성후 이동하는 로직을 테스트하는건 더 생각해봐야 함.

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,10 +1,42 @@
 package racingcar;
 
+import java.util.Random;
+
 public class Car {
     private final String name;
     private int position = 0;
 
     public Car(String name) {
         this.name = name;
+    }
+
+    public Car(String name, int position) {
+        this.name = name;
+        this.position = position;
+    }
+
+
+    private int pickRandomNumber() {
+        Random rand = new Random();
+        rand.setSeed(System.currentTimeMillis());
+        return rand.nextInt(10);
+    }
+
+    public void go() {
+        if (pickRandomNumber() >= 4) {
+            this.position++;
+        }
+    }
+
+    public void printPosition() {
+        StringBuilder sb = new StringBuilder(name);
+        sb.append(" : ");
+        String step = "-";
+        sb.append(step.repeat(position));
+        System.out.println(sb);
+    }
+
+    public int getPosition() {
+        return this.position;
     }
 }
---
+++ src/test/java/racingcar/CarTest.java
@@ -0,0 +1,31 @@
+package racingcar;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class CarTest {
+
+    private ByteArrayOutputStream outputStream;
+
+    @BeforeEach
+    public void outputSetUp() {
+        outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    public String getOutput() {
+        return outputStream.toString();
+    }
+
+    @Test
+    public void printPositionTest() {
+        Car car = new Car("abc", 3);
+        car.printPosition();
+        String output = getOutput();
+        Assertions.assertThat(output).contains("abc : ---");
+    }
+}

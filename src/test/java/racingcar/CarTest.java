c401d9fba7b4425bd9dfc3d12c4c91966c5d1d8d
euichanhwang <pine6971@naver.com>
2023-01-27 12:43:08 +0900

    refactor : BeforeEach를 사용해서 CarTest 리팩토링
    매 테스트마다 Car를 생성하도록 BeforeEach를 구현했습니다.

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -2,21 +2,30 @@ package racingcar;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import racingcar.model.Car;
+
 class CarTest {
+
+	private Car car;
+
+	@BeforeEach
+	void initiateCar() {
+		car = new Car("chan");
+	}
+
 	@Test
 	@DisplayName("자동차 생성 테스트")
 	void create_car() {
-		Car car = new Car("chan");
 		assertThat(car).isNotNull();
 	}
 
 	@Test
 	@DisplayName("자동차 이름 검증 테스트")
 	void validate_car_name() {
-		Car car = new Car("chan");
 		assertThat(car.getName()).isEqualTo("chan");
 	}
 }

aed477a76d138059297533f38a0b1fc10d6644c2
euichanhwang <pine6971@naver.com>
2023-01-27 15:15:42 +0900

    feat : 사용자 입력 횟수 예외처리 구현
    요구사항에 맞게 예외처리를 진행했습니다.

---
+++ src/main/java/racingcar/util/validator/NumberValidator.java
@@ -0,0 +1,27 @@
+package racingcar.util.validator;
+
+public class NumberValidator {
+
+	private static final String MIN_ATTEMPT_NUMBER_MESSAGE = "시도 횟수는 1회 이상이어야 합니다.";
+	private static final String ATTEMPT_NUMBER_IS_NOT_INTEGER = "시도 횟수는 숫자여야 한다.";
+
+	public static void validateNumber(String attemptNumber) {
+		checkNumberIsNotInteger(attemptNumber);
+		checkNumberRange(attemptNumber);
+	}
+
+	private static void checkNumberIsNotInteger(String attemptNumber) {
+		try {
+			Integer.parseInt(attemptNumber);
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException(ATTEMPT_NUMBER_IS_NOT_INTEGER);
+		}
+	}
+
+	private static void checkNumberRange(String attemptNumber) {
+		int attemptNumberToInteger = Integer.parseInt(attemptNumber);
+		if (attemptNumberToInteger < 1) {
+			throw new IllegalArgumentException(MIN_ATTEMPT_NUMBER_MESSAGE);
+		}
+	}
+}
--- src/test/java/racingcar/util/validator/NumberValidatorTest.java
+++ src/test/java/racingcar/util/validator/NumberValidatorTest.java
@@ -1,5 +1,25 @@
 package racingcar.util.validator;
 
-public class NumberValidatorTest {
-	
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class NumberValidatorTest {
+
+	private static final String ZERO = "0";
+
+	@ParameterizedTest(name = "{index} : 시도 횟수 {0}번")
+	@ValueSource(strings = {"-1", "0"})
+	void 시도횟수_범위(String input) {
+		assertThatThrownBy(() -> NumberValidator.validateNumber(input))
+			.isInstanceOf(RuntimeException.class);
+	}
+
+	@ParameterizedTest(name = "{index} : 숫자가 아닌 문자 {0} 입력")
+	@ValueSource(strings = {"a", "B", "input"})
+	void 숫자_미입력(String input) {
+		assertThatThrownBy(() -> NumberValidator.validateNumber(input))
+			.isInstanceOf(RuntimeException.class);
+	}
 }

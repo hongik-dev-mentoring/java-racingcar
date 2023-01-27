42aacac3ba3f9cbd0d2ba5520875c96f2406c028
euichanhwang <pine6971@naver.com>
2023-01-27 09:52:31 +0900

    test : 쉼표 구분자, 쉼표 또는 콜론 구분자에 대한 테스트 작성
    실제 코드를 작성하기 전에 첫 번째 요구사항인
    쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열을 전달하는 경우 구분자를 기준으로 분리한 각 숫자의 합을 반환
    에 대한 테스트를 먼저 작성하였습니다.

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,5 +1,20 @@
 package calculator;
 
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
 class CalculatorTest {
+	@Test
+	public void splitAndSum_쉼표구분자() throws Exception {
+		int result = Calculator.splitAndSum("1,2");
+		assertThat(result).isEqualTo(3);
+	}
+
+	@Test
+	public void spliatAndSum_쉼표_또는_콜론_구분자() throws Exception {
+		int result = Calculator.splitAndSum("1,2:3");
+		assertThat(result).isEqualTo(6);
+	}
 	
 }

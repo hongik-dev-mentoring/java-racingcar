749bac5abca1a2d7c147c6c0061a7e7226403614
Junek3 <rlarudrms28@gmail.com>
2023-01-29 04:57:42 +0900

    refactor: 코드 리뷰때 지적받은 부분 수정

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -13,26 +13,32 @@ public class Calculator {
         if (isInputNullOrEmpty(input)) {
             return 0;
         }
-        if (CustomSeparatorExists(input)) {
+        if (customSeparatorExists(input)) {
             return calculateWithCustomDelimiter(input);
         }
         if (isInputOneNumberString(input)) {
-            return calcuateOneNumberInputString(input);
+            return calculateOneNumberInputString(input);
         }
         return calculateWithDefaultSeparator(input);
     }
 
-    private int calcuateOneNumberInputString(String input) {
-        String result = splitInputString(input)[0];
-        return Integer.parseInt(result);
+    private boolean isInputNullOrEmpty(String input) {
+        return input == null || input.isEmpty() || input.isBlank();
+    }
+
+    private boolean customSeparatorExists(String input) {
+        return input.matches(customDelimiterPattern);
     }
 
     private int calculateWithCustomDelimiter(String input) {
-        Matcher m = Pattern.compile(customDelimiterPattern).matcher(input);
+        Matcher m = Pattern.compile(customDelimiterPattern)
+                .matcher(input); // 이 부분도 고쳐야 함
         String[] tokens = new String[0];
         if (m.find()) {
             String customDelimiter = m.group(1);
-            tokens = m.group(2).split(customDelimiter);
+            tokens = m.group(2)
+                    .split(customDelimiter);
+            Arrays.stream(tokens).forEach(System.out::println);
         }
         checkTokensAreInvalid(tokens);
         return calculateSum(tokens);
@@ -43,29 +49,6 @@ public class Calculator {
             Arrays.stream(tokens).map(s -> Integer.parseInt(s.trim()));
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("지정된 커스텀 구분자로 숫자를 구분해야 합니다.");
-        }
-    }
-
-    private int calculateWithDefaultSeparator(String input) {
-        String[] tokens = splitInputString(input);
-        checkNegativeNumberNotExists(tokens);
-        return calculateSum(tokens);
-    }
-
-    public boolean CustomSeparatorExists(String input) {
-        return input.matches(customDelimiterPattern);
-    }
-
-    private void checkNegativeNumberNotExists(String[] tokens) {
-        Arrays.stream(tokens).forEach(s -> {
-            int number = Integer.parseInt(s.trim());
-            checkIfNumberIsNegative(number);
-        });
-    }
-
-    private void checkIfNumberIsNegative(int number) {
-        if (number < 0) {
-            throw new RuntimeException("음수는 계산할 수 없습니다.");
         }
     }
 
@@ -84,7 +67,28 @@ public class Calculator {
         return input.trim().split(defaultDelimiterPattern);
     }
 
-    private boolean isInputNullOrEmpty(String input) {
-        return input == null || input.isEmpty() || input.isBlank();
+    private int calculateOneNumberInputString(String input) {
+        String result = splitInputString(input)[0];
+        return Integer.parseInt(result);
     }
+
+    private int calculateWithDefaultSeparator(String input) {
+        String[] tokens = splitInputString(input);
+        checkNegativeNumberNotExists(tokens);
+        return calculateSum(tokens);
+    }
+
+    private void checkNegativeNumberNotExists(String[] tokens) {
+        Arrays.stream(tokens).forEach(s -> {
+            int number = Integer.parseInt(s.trim());
+            checkIfNumberIsNegative(number);
+        });
+    }
+
+    private void checkIfNumberIsNegative(int number) {
+        if (number < 0) {
+            throw new RuntimeException("음수는 계산할 수 없습니다.");
+        }
+    }
+
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -2,24 +2,22 @@ package calculator;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.Arrays;
-import java.util.List;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CalculatorTest {
-    @Test
+
     @DisplayName("빈 문자열을 입력할 경우 0을 반환한다.")
-    public void emptyInputTest() {
+    @ParameterizedTest(name = "#{index} {displayName}")
+    @ValueSource(strings = {"", "   "})
+    public void emptyInputTest(String input) {
         // given
         Calculator calculator = new Calculator();
         // when & then
-        int actual = calculator.splitAndSum("");
-        assertThat(actual).isEqualTo(0);
-        // when & then
-        actual = calculator.splitAndSum("   ");
+        int actual = calculator.splitAndSum(input);
         assertThat(actual).isEqualTo(0);
     }
 
@@ -34,17 +32,15 @@ public class CalculatorTest {
         assertThat(actual).isEqualTo(0);
     }
 
-    @Test
     @DisplayName("숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.")
-    public void oneNumberStringInputTest() {
+    @ParameterizedTest(name = "#{index} {displayName}")
+    @ValueSource(strings = {"7", " 7", "7 ", "7,", "7:"})
+    public void oneNumberStringInputTest(String input) {
         // given
         Calculator calculator = new Calculator();
-        List<String> testData = Arrays.asList("7", " 7", "7 ", "7,", "7:");
         // when & then
-        testData.forEach(input -> {
-            int actual = calculator.splitAndSum(input);
-            assertThat(actual).isEqualTo(7);
-        });
+        int actual = calculator.splitAndSum(input);
+        assertThat(actual).isEqualTo(7);
     }
 
     @Test
@@ -83,30 +79,20 @@ public class CalculatorTest {
         assertThat(actual).isEqualTo(6);
     }
 
-    @Test
-    public void testCustomSeparatorExistsMethod() {
-        // given
-        Calculator calculator = new Calculator();
-        String input = "//.\n1.2.3";
-        // when
-        boolean exists = calculator.CustomSeparatorExists(input);
-        // then
-        assertThat(exists).isTrue();
-    }
-
-    @Test
     @DisplayName("'//'와 '\n' 문자 사이에 커스텀 구분자를 지정할 수 있다.")
-    public void numbersWithCustomSeparatorInputTest() {
+    @ParameterizedTest(name = "#{index} {displayName}")
+    @ValueSource(strings = {"//;\n1;2;3", "//-\n1-2-3"})
+    public void numbersWithCustomSeparatorInputTest(String input) {
         // given
         Calculator calculator = new Calculator();
         // when
-        int actual = calculator.splitAndSum("//;\n1;2;3");
-//        int actual = calculator.splitAndSum("//;\n1:2:3");
+        int actual = calculator.splitAndSum(input);
         // then
         assertThat(actual).isEqualTo(6);
     }
 
     @Test
+    @DisplayName("지정한 커스텀 구분자로 숫자를 입력하지 않는 경우 예외 발생")
     public void customSeparatorInvalidInputTest() {
         // given
         Calculator calculator = new Calculator();
@@ -124,6 +110,6 @@ public class CalculatorTest {
         // when & then
         assertThatThrownBy(() -> calculator.splitAndSum(input))
                 .isInstanceOf(RuntimeException.class)
-                .hasMessageContaining("음수는 계산할 수 없습니다.");
+                .hasMessage("음수는 계산할 수 없습니다.");
     }
 }

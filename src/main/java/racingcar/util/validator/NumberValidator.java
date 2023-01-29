f999442fcf190f84cbafcdf626d216feb154c631
euichanhwang <pine6971@naver.com>
2023-01-29 18:32:29 +0900

    refactor : 매직 넘버를 피하기
    매직넘버의 사용은 코드의 가독성을 어렵게 만드므로 상수를 사용했습니다.

--- src/main/java/racingcar/util/validator/NumberValidator.java
+++ src/main/java/racingcar/util/validator/NumberValidator.java
@@ -4,6 +4,7 @@ public class NumberValidator {
 
 	private static final String MIN_ATTEMPT_NUMBER_MESSAGE = "시도 횟수는 1회 이상이어야 합니다.";
 	private static final String ATTEMPT_NUMBER_IS_NOT_INTEGER = "시도 횟수는 숫자여야 한다.";
+	private static final int MIN_ATTEMPT_NUMBER = 1;
 
 	public static void validateNumber(String attemptNumber) {
 		checkNumberIsNotInteger(attemptNumber);
@@ -20,7 +21,7 @@ public class NumberValidator {
 
 	private static void checkNumberRange(String attemptNumber) {
 		int attemptNumberToInteger = Integer.parseInt(attemptNumber);
-		if (attemptNumberToInteger < 1) {
+		if (attemptNumberToInteger < MIN_ATTEMPT_NUMBER) {
 			throw new IllegalArgumentException(MIN_ATTEMPT_NUMBER_MESSAGE);
 		}
 	}

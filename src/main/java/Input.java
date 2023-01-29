29ea74f11412694c48e061c73436e56d3f54dd8c
LTSGOD <lts0424@naver.com>
2023-01-29 20:37:32 +0900

    chore(Input): 코딩 컨벤션 수정
    - 메서드명이 동사로 시작하게 수정

--- src/main/java/Input.java
+++ src/main/java/Input.java
@@ -10,7 +10,7 @@ public class Input {
 		System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
 		while (true) {
 			String input = sc.nextLine();
-			if (noExceptionAtStrings(input))
+			if (!isExceptionAtStrings(input))
 				break;
 		}
 		return strings;
@@ -40,7 +40,7 @@ public class Input {
 		}
 	}
 
-	private static boolean noExceptionAtStrings(String input) {
+	private static boolean isExceptionAtStrings(String input) {
 		try {
 			RacingCarInputException.isNull(input);
 			RacingCarInputException.isEmpty(input);

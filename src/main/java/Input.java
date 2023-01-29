cc600848c31abb78ed9647f8bd8e664e042b9db8
LTSGOD <lts0424@naver.com>
2023-01-29 19:59:39 +0900

    chore(Input): 코딩 컨벤션 수정
    - 상수 코딩 컨벤션 수정

--- src/main/java/Input.java
+++ src/main/java/Input.java
@@ -3,48 +3,50 @@ import java.util.Scanner;
 
 public class Input {
 	private static ArrayList<String> strings = new ArrayList<>();
-	private static int tryNum;
+	private static int TRY_NUM;
 
-	public static ArrayList<String> readNames(){
+	public static ArrayList<String> readNames() {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-		while(true){
+		while (true) {
 			String input = sc.nextLine();
-			if (noExceptionAtStrings(input)) break;
+			if (noExceptionAtStrings(input))
+				break;
 		}
 		return strings;
 	}
 
-	public static int readNum(){
+	public static int readNum() {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("시도할 회수는 몇회인가요?");
-		while(true){
+		while (true) {
 			String input = sc.nextLine();
-			if(noExceptionAtNum(input)) break;
+			if (noExceptionAtNum(input))
+				break;
 		}
-		return tryNum;
+		return TRY_NUM;
 	}
 
 	private static boolean noExceptionAtNum(String input) {
-		try{
-			InputException.ifEmpty(input);
-			InputException.ifNull(input);
-			tryNum = Integer.parseInt(input);
-			InputException.isPositive(tryNum);
+		try {
+			RacingCarInputException.isEmpty(input);
+			RacingCarInputException.isNull(input);
+			TRY_NUM = Integer.parseInt(input);
+			RacingCarInputException.isPositive(TRY_NUM);
 			return true;
-		}catch(Exception e){
+		} catch (Exception e) {
 			System.out.println("숫자를 입력하세요.");
 			return false;
 		}
 	}
 
 	private static boolean noExceptionAtStrings(String input) {
-		try{
-			InputException.ifNull(input);
-			InputException.ifEmpty(input);
-			InputException.ifLong(strings, input.split(","));
+		try {
+			RacingCarInputException.isNull(input);
+			RacingCarInputException.isEmpty(input);
+			RacingCarInputException.isLong(strings, input.split(","));
 			return true;
-		}catch(IllegalArgumentException e){
+		} catch (IllegalArgumentException e) {
 			System.out.println(e.getMessage());
 			return false;
 		}

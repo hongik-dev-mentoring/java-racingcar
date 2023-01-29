68d98b8844d673c1951862f04daac3c9bceed277
LTSGOD <lts0424@naver.com>
2023-01-29 20:00:55 +0900

    chore(RacingCarInputException): 코딩 컨벤션 수정
    - 메서드이름 동사로 시작

--- src/main/java/RacingCarInputException.java
+++ src/main/java/RacingCarInputException.java
@@ -1,19 +1,19 @@
 import java.util.ArrayList;
 
-public class InputException {
+public class RacingCarInputException {
 	private static final int MAX_LENGTH = 5;
 
-	public static void ifEmpty(String input) {
+	public static void isEmpty(String input) {
 		if (input.isEmpty())
 			throw new IllegalArgumentException("이름을 입력해주세요.");
 	}
 
-	public static void ifNull(String input) {
+	public static void isNull(String input) {
 		if (input == null)
 			throw new IllegalArgumentException("다시 입력해주세요.");
 	}
 
-	public static void ifLong(ArrayList<String> strings, String[] input) {
+	public static void isLong(ArrayList<String> strings, String[] input) {
 		for (String s : input) {
 			if (s.length() > MAX_LENGTH)
 				throw new IllegalArgumentException("5이하의 문자열을 입력하세요.");

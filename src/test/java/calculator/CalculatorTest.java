2496719a413f8e8d34e2d4c1f27dd5a5fdae3765
euichanhwang <pine6971@naver.com>
2023-01-29 17:14:12 +0900

    refactor : throws Exception 제거
    테스트 메서드에서 예외를 발생시키지 않으므로 throws Exception을 제거했습니다.

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -6,49 +6,49 @@ import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
 	@Test
-	public void splitAndSum_쉼표구분자() throws Exception {
+	public void splitAndSum_쉼표구분자() {
 		int result = Calculator.splitAndSum("1,2");
 		assertThat(result).isEqualTo(3);
 	}
 
 	@Test
-	public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
+	public void splitAndSum_쉼표_또는_콜론_구분자() {
 		int result = Calculator.splitAndSum("1,2:3");
 		assertThat(result).isEqualTo(6);
 	}
 
 	@Test
-	public void splitAndSum_custom_구분자() throws Exception {
+	public void splitAndSum_custom_구분자() {
 		int result = Calculator.splitAndSum("//;\n1;2;3");
 		assertThat(result).isEqualTo(6);
 	}
 
 	@Test
-	public void splitAndSum_null() throws Exception {
+	public void splitAndSum_null() {
 		int result = Calculator.splitAndSum(null);
 		assertThat(result).isEqualTo(0);
 	}
 
 	@Test
-	public void splitAndSum_빈문자() throws Exception {
+	public void splitAndSum_빈문자() {
 		int result = Calculator.splitAndSum("");
 		assertThat(result).isEqualTo(0);
 	}
 
 	@Test
-	public void splitAndSum_숫자하나() throws Exception {
+	public void splitAndSum_숫자하나() {
 		int result = Calculator.splitAndSum("1");
 		assertThat(result).isEqualTo(1);
 	}
 
 	@Test
-	public void splitAndSum_negative() throws Exception {
+	public void splitAndSum_negative() {
 		assertThatThrownBy(() -> Calculator.splitAndSum("-1,-2,-3"))
 			.isInstanceOf(RuntimeException.class);
 	}
 
 	@Test
-	public void splitAndSum_숫자이외의값() throws Exception {
+	public void splitAndSum_숫자이외의값() {
 		assertThatThrownBy(() -> Calculator.splitAndSum("a, b, C"))
 			.isInstanceOf(RuntimeException.class);
 	}

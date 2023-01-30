62f59feb7418f239073dae0ebd13967b10ad9446
euichanhwang <pine6971@naver.com>
2023-01-30 22:37:35 +0900

    refactor : 클래스를 정의하는 표준 자바 관례에 따라 가독성 높이기
    정적 공개(public) 상수, 정적 비공개 변수, 비공개 인스턴스 변수 순으로 나오도록 리팩토링했습니다.

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,9 +1,9 @@
 package racingcar.model;
 
 public class Car {
+	private static final int MOVE_FORWARD_RANDOM_NUMBER_STANDARD = 4;
 	private final String name;
 	private int position = 0;
-	private static final int MOVE_FORWARD_RANDOM_NUMBER_STANDARD = 4;
 
 	public Car(String name) {
 		this.name = name;
--- src/test/java/racingcar/util/validator/NameValidatorTest.java
+++ src/test/java/racingcar/util/validator/NameValidatorTest.java
@@ -11,14 +11,13 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class NameValidatorTest {
-	private final List<String> emptyNames = List.of("");
-	private final List<String> duplicateNames = List.of("chan", "chan");
-
-	private final List<String> exceedNames = List.of("euichan");
 	private static final String NAME_LENGTH_EXCEPTION_MESSAGE = "자동차의 이름은 1자 이상 5자 이하여야 합니다.";
 	private static final String CAR_NAME_NOT_EXIST_MESSAGE = "자동차의 이름이 존재하지 않습니다.";
 	private static final String DUPLICATE_CAR_NAME_MESSAGE = "자동차의 이름은 중복일 수 없습니다.";
-
+	private final List<String> emptyNames = List.of("");
+	private final List<String> duplicateNames = List.of("chan", "chan");
+	private final List<String> exceedNames = List.of("euichan");
+	
 	@Test
 	@DisplayName("자동차 이름은 Null일 수 없다.")
 	void car_name_should_not_be_null() {

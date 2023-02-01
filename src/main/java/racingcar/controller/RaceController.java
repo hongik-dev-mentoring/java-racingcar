9b2db7e2785a04f0bcb4b947cab2fb552db6447d
Junek3 <rlarudrms28@gmail.com>
2023-02-01 17:45:14 +0900

    feat: RaceController 작성하기

---
+++ src/main/java/racingcar/controller/RaceController.java
@@ -0,0 +1,45 @@
+package racingcar.controller;
+
+import racingcar.model.Car;
+import racingcar.model.Race;
+import racingcar.view.InputProcess;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class RaceController {
+
+    private static final String COMMA_IN_RESULT_TEXT = ", ";
+    private static final String RESULT_TEXT = "실행 결과";
+    private static final String WINNER_TEXT = "가 최종 우승했습니다.";
+
+    private Race race;
+
+    public RaceController() {
+        executeInputProcess();
+    }
+
+    private void executeInputProcess() {
+        InputProcess inputProcess = new InputProcess();
+        List<Car> cars = inputProcess.getCarList();
+        Integer moveCount = inputProcess.getMoveCount();
+        race = new Race(cars, moveCount);
+    }
+
+    public void startRace() {
+        System.out.println(RESULT_TEXT);
+        race.proceedWholeRound();
+    }
+
+    public void printWinners() {
+        List<Car> winners = race.selectWinners();
+        createFinalResultText(winners);
+    }
+
+    private void createFinalResultText(List<Car> winners) {
+        List<String> winnerNames = winners.stream()
+                .map(Car::getName)
+                .collect(Collectors.toList());
+        System.out.println(String.join(COMMA_IN_RESULT_TEXT, winnerNames) + WINNER_TEXT);
+    }
+}
---
+++ src/test/java/racingcar/controller/RaceControllerTest.java
@@ -0,0 +1,65 @@
+package racingcar.controller;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RaceControllerTest {
+    private ByteArrayOutputStream outputStream;
+
+    @BeforeEach
+    public void outputSetUp() {
+        outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    @BeforeEach
+    public void inputSetUp() {
+        String input = "pobi,crong,honux\n3\n";
+        systemIn(input);
+    }
+
+    public void systemIn(String input) {
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+    }
+
+    @Test
+    @DisplayName("자동차 경주 실행결과 출력 테스트")
+    public void raceStartTest() {
+        // given
+        RaceController raceController = new RaceController();
+
+        // when
+        raceController.startRace();
+        String output = getOutput();
+
+        // then
+        assertThat(output).contains("실행 결과");
+    }
+
+    @Test
+    @DisplayName("자동차 경주 우승자 출력 테스트")
+    public void printWinnersTest() {
+        // given
+        RaceController raceController = new RaceController();
+
+        // when
+        raceController.startRace();
+        raceController.printWinners();
+        String output = getOutput();
+
+        // then
+        Assertions.assertThat(output).contains("가 최종 우승했습니다.");
+    }
+
+    public String getOutput() {
+        return outputStream.toString();
+    }
+}

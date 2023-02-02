a478eb9c43d904db1ba353941ece9da5fd12176f
euichanhwang <pine6971@naver.com>
2023-02-02 16:48:07 +0900

    refactor : Displaynamegeneration 삭제
    underscore 함수가 없으므로 불필요한 DisplaynameGeneration 애노테이션을 제거했습니다.

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,16 +1,13 @@
 package racingcar.domain;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.DisplayNameGenerator.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-@DisplayNameGeneration(ReplaceUnderscores.class)
 class CarTest {
 	private Car car;
 

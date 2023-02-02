538c1961c2690bf2e07d6900d22a578fb2f00c19
LTSGOD <lts0424@naver.com>
2023-02-02 13:42:50 +0900

    refactor: Random 수 생성 기능 Car와 분리 및 테스트 완료

--- src/main/java/racingcar/util/RandomNum.java
+++ src/main/java/racingcar/util/RandomNum.java
@@ -1,2 +1,9 @@
-package racingcar;public class RandomNum {
+package racingcar.util;
+
+public class RandomNum {
+	private static final int ZERO_TO_NINE_CONVERTER = 10;
+
+	public static int createRandomNum() {
+		return (int)(Math.random() * ZERO_TO_NINE_CONVERTER);
+	}
 }

387cb3dc1eb51dcd3553d4eed6889d437ae645b9
LTSGOD <lts0424@naver.com>
2023-02-02 14:34:56 +0900

    feat: 예외처리 테스트 추가

---
+++ src/test/java/racingcar/view/RacingCarInputExceptionTest.java
@@ -0,0 +1,54 @@
+package racingcar.view;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RacingCarInputExceptionTest {
+
+	@Test
+	@DisplayName("null 값 테스트")
+	void isNull(){
+		String test = null;
+
+		Assertions.assertThrows(IllegalArgumentException.class, () -> RacingCarInputException.isNull(test));
+	}
+
+	@Test
+	@DisplayName("Empty 값 테스트")
+	void isEmpty(){
+		String test = "";
+
+		Assertions.assertThrows(IllegalArgumentException.class, () -> RacingCarInputException.isEmpty(test));
+	}
+
+	@Test
+	@DisplayName("5이하의 문자열 입력 테스트")
+	void isLessThanFive(){
+		String[] test = {"na","tae","abcdef"};
+		ArrayList<String> result = new ArrayList<>();
+
+		assertAll(
+			()-> assertThrows(IllegalArgumentException.class, () -> RacingCarInputException.checkValidation(result,test)),
+			()-> assertThat(result.size()).isEqualTo(2)
+		);
+	}
+
+	@Test
+	@DisplayName("중복 이름 테스트")
+	void isDuplicatedName(){
+		String[] test = {"na","tae","joe","tae"};
+		ArrayList<String> result = new ArrayList<>();
+
+		assertAll(
+			()-> assertThrows(IllegalArgumentException.class, () -> RacingCarInputException.checkValidation(result, test)),
+			()-> assertThat(result.size()).isEqualTo(3)
+		);
+	}
+
+}

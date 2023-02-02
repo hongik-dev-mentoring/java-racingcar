aab934c844338fdde8505200efabc75de2172417
LTSGOD <lts0424@naver.com>
2023-02-02 13:43:54 +0900

    fix(InputView): Input 버그 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -12,7 +12,7 @@ public class InputView {
 		System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
 		while (true) {
 			String input = sc.nextLine();
-			if (!isExceptionAtStrings(input)) {
+			if (isNoExceptionAtStrings(input)) {
 				break;
 			}
 		}
@@ -24,14 +24,14 @@ public class InputView {
 		System.out.println("시도할 회수는 몇회인가요?");
 		while (true) {
 			String input = sc.nextLine();
-			if (noExceptionAtNum(input)) {
+			if (isNoExceptionAtNum(input)) {
 				break;
 			}
 		}
 		return TRY_NUM;
 	}
 
-	private static boolean noExceptionAtNum(String input) {
+	private static boolean isNoExceptionAtNum(String input) {
 		try {
 			RacingCarInputException.isEmpty(input);
 			RacingCarInputException.isNull(input);
@@ -44,7 +44,7 @@ public class InputView {
 		}
 	}
 
-	private static boolean isExceptionAtStrings(String input) {
+	private static boolean isNoExceptionAtStrings(String input) {
 		try {
 			RacingCarInputException.isNull(input);
 			RacingCarInputException.isEmpty(input);

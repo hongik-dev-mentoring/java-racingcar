3fc0c3d1b4ddcdd732bc7e9f2b31da95fdd6003a
euichanhwang <pine6971@naver.com>
2023-02-05 00:57:59 +0900

    refactor : AttemptNumberValidator로 클래스명 변경
    미션에는 자동차의 위치와 시도 횟수라는 Number가 있는데, NumberValidator라는 클래스명보다는
    시도 횟수 검증에 관한 클래스이므로 AttemptNumberValidator라는 이름이 더 가독성과 유지보수에 좋다고 생각해서
    변경했습니다.

--- src/main/java/racingcar/util/convertor/NumberConvertor.java
+++ src/main/java/racingcar/util/convertor/NumberConvertor.java
@@ -1,10 +1,10 @@
 package racingcar.util.convertor;
 
-import racingcar.util.validator.NumberValidator;
+import racingcar.util.validator.AttemptNumberValidator;
 
 public class NumberConvertor {
 	public static int convertNumber(String attemptNumber) {
-		NumberValidator.validateNumber(attemptNumber);
+		AttemptNumberValidator.validateNumber(attemptNumber);
 		return Integer.parseInt(attemptNumber);
 	}
 }
--- src/test/java/racingcar/util/validator/NumberValidatorTest.java
+++ src/test/java/racingcar/util/validator/NumberValidatorTest.java
@@ -9,14 +9,14 @@ class NumberValidatorTest {
 	@ParameterizedTest(name = "{index} : 시도 횟수 {0}번")
 	@ValueSource(strings = {"-1", "0"})
 	void 시도횟수_범위(String input) {
-		assertThatThrownBy(() -> NumberValidator.validateNumber(input))
+		assertThatThrownBy(() -> AttemptNumberValidator.validateNumber(input))
 			.isInstanceOf(IllegalArgumentException.class);
 	}
 
 	@ParameterizedTest(name = "{index} : 숫자가 아닌 문자 {0} 입력")
 	@ValueSource(strings = {"a", "B", "input"})
 	void 숫자_미입력(String input) {
-		assertThatThrownBy(() -> NumberValidator.validateNumber(input))
+		assertThatThrownBy(() -> AttemptNumberValidator.validateNumber(input))
 			.isInstanceOf(IllegalArgumentException.class);
 	}
 }

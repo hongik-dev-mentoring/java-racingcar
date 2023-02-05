c30ff91b18b29f9c6fc8fd1eb26f2730c1baca2a
Jinho <dfghcvb11@naver.com>
2023-02-05 18:34:36 +0900

    test: 레이싱 공동 우승자 처리 테스트가 view를 의존하지 않도록 수정

--- src/test/java/racingcar/domain/RacingTest.java
+++ src/test/java/racingcar/domain/RacingTest.java
@@ -1,35 +1,15 @@
 package racingcar.domain;
 
-import static java.util.List.of;
 import static org.assertj.core.api.Assertions.assertThat;
-import static racingcar.view.CarRacingResultView.printCurrentRaceResult;
-import static racingcar.view.CarRacingResultView.printRacingGameWinner;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 import java.util.LinkedList;
 import java.util.List;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import racingcar.domain.movingstrategy.AlwaysMovingStrategy;
 import racingcar.domain.movingstrategy.MovingStrategy;
 import racingcar.view.CarNames;
 
 public class RacingTest {
-
-    private final ByteArrayOutputStream output = new ByteArrayOutputStream();
-
-    @BeforeEach
-    void setOutPrintStream() {
-        System.setOut(new PrintStream(output));
-    }
-
-    @AfterEach
-    void resetOutputStream() {
-        System.setOut(System.out);
-        output.reset();
-    }
 
     @Test
     void 레이싱을_할_수_있다() {
@@ -55,19 +35,23 @@ public class RacingTest {
     @Test
     void 레이싱_공동_우승자_처리_테스트() {
         // given
-        CarNames carNames = CarNames.createCarNamesFromCarNamesString("jinh1, jinh2");
+        List<String> carNamesList = new LinkedList<>();
+        carNamesList.add("jinh1");
+        carNamesList.add("jinh2");
+
+        CarNames carNames = CarNames.createCarNamesFromCarNamesString(String.join(",", carNamesList));
         Cars cars = carNames.createCars();
         Racing racing = new Racing(cars, 3);
 
         MovingStrategy movingStrategy = new AlwaysMovingStrategy();
-        StringBuilder expectedOutputString = new StringBuilder("최종 우승자 : jinh1, jinh2\r\n");
+        List<String> expectedResult =  new LinkedList<>(carNamesList);
 
         // when
         racing.raceAllCar(movingStrategy);
-        printRacingGameWinner(racing);
-        String actualOutputString = output.toString();
+        int leadCarPosition = racing.getCars().getLeadCarPosition();
+        List<String> carsAfterRace = racing.getCars().getCarsByPosition(leadCarPosition);
 
         // then
-        assertThat(actualOutputString).contains(expectedOutputString);
+        assertThat(carsAfterRace).isEqualTo(expectedResult);
     }
 }

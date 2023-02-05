9656130cc0b9bfd6969eb0a41c2428e57a07343f
Jinho <dfghcvb11@naver.com>
2023-02-05 09:40:34 +0900

    test: 레이싱 결과를 출력하는 테스트 코드 작성

---
+++ src/test/java/racingcar/view/CarRacingResultViewTest.java
@@ -0,0 +1,50 @@
+package racingcar.view;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static racingcar.view.CarRacingResultView.printCurrentRaceResult;
+import static racingcar.view.CarRacingResultView.printRacingGameWinner;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.LinkedList;
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
+import racingcar.domain.Racing;
+
+class CarRacingResultViewTest {
+
+    private final ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+    @BeforeEach
+    void setOutPrintStream() {
+        System.setOut(new PrintStream(output));
+    }
+
+    @AfterEach
+    void resetOutputStream() {
+        System.setOut(System.out);
+        output.reset();
+    }
+
+    @Test
+    void 레이싱_결과를_출력_할_수_있다() {
+        List<Car> carList = new LinkedList<>();
+        carList.add(new Car("jinho"));
+
+        Cars cars = new Cars(carList);
+        int gameCount = 0;
+        Racing racing = new Racing(cars, gameCount);
+        printCurrentRaceResult(racing);
+
+        StringBuilder expectedOutput = new StringBuilder();
+        expectedOutput.append("jinho : \r\n");
+
+        String actualOutput = output.toString();
+        assertThat(actualOutput).contains(expectedOutput);
+    }
+}

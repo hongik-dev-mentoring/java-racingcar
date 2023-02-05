86a5cfdf2a5a50fa006d0e6f9047870b91928c27
Jinho <dfghcvb11@naver.com>
2023-02-05 17:14:13 +0900

    chore: 변수명 오타 수정

--- src/test/java/racingcar/RangedRandomNumberPickerTest.java
+++ src/test/java/racingcar/RangedRandomNumberPickerTest.java
@@ -7,19 +7,18 @@ import racingcar.domain.movingstrategy.RangedRandomNumberPicker;
 
 public class RangedRandomNumberPickerTest {
 
-    int form = 0;
+    int from = 0;
     int to = 9;
 
     @RepeatedTest(100)
-
     void 범위내의_무작위_숫자를_뽑을_수_있다() {
         // given
-        RangedRandomNumberPicker randomNumberPicker = new RangedRandomNumberPicker(form, to);
+        RangedRandomNumberPicker randomNumberPicker = new RangedRandomNumberPicker(from, to);
 
         // when
         int result = randomNumberPicker.pickRandomNumber();
 
         // then
-        assertThat(result).isGreaterThanOrEqualTo(form).isLessThanOrEqualTo(to);
+        assertThat(result).isGreaterThanOrEqualTo(from).isLessThanOrEqualTo(to);
     }
 }

50012b14dc9305312e76311b9777a40115a7144f
LTSGOD <lts0424@naver.com>
2023-02-06 12:30:00 +0900

    refactor: test형식 통일, 변수 이름 대문자-> 소문자 변경, 중복 부분 픽스처로 분리

--- src/test/java/racingcar/domain/RacingGameTest.java
+++ src/test/java/racingcar/domain/RacingGameTest.java
@@ -13,58 +13,57 @@ import org.junit.jupiter.api.Test;
 
 class RacingGameTest {
 
-	RacingGame game;
-	final ArrayList<String> NAMES = new ArrayList<>(Arrays.asList("na", "tae", "joe"));
+    RacingGame game;
+    final ArrayList<String> names = new ArrayList<>(Arrays.asList("na", "tae", "joe"));
 
-	@BeforeEach
-	void setup() {
-		game = new RacingGame(NAMES);
-	}
+    @BeforeEach
+    void setup() {
+        game = new RacingGame(names);
+        game.createCars(new ArrayList<>(Arrays.asList("na", "tae", "joe")));
+        for (int i = 0; i < 3; i++) {
+            game.race();
+        }
+    }
 
-	@Test
-	@DisplayName("입력받고 Car객체 배열 생성")
-	void createCars() {
-		//given
-		ArrayList<String> carNames = new ArrayList<>(Arrays.asList("nae", "tae", "joe"));
+    @Test
+    @DisplayName("입력받고 Car객체 배열 생성")
+    void createCars() {
+        //given
+        ArrayList<String> carNames = new ArrayList<>(Arrays.asList("nae", "tae", "joe"));
 
-		//when
-		List<Car> cars = game.createCars(carNames);
+        //when
+        List<Car> cars = game.createCars(carNames);
 
-		//then
-		assertAll(
-			() -> assertThat(cars.get(0).getName()).isEqualTo("nae"),
-			() -> assertThat(cars.get(1).getName()).isEqualTo("tae"),
-			() -> assertThat(cars.get(2).getName()).isEqualTo("joe")
-		);
+        //then
+        assertAll(
+                () -> assertThat(cars.get(0).getName()).isEqualTo("nae"),
+                () -> assertThat(cars.get(1).getName()).isEqualTo("tae"),
+                () -> assertThat(cars.get(2).getName()).isEqualTo("joe")
+        );
 
-	}
+    }
 
-	@Test
-	@DisplayName("최종 position을 기준으로 승자 정렬.")
-	void rankFinal() {
-		//given
-		List<Car> cars = game.createCars(new ArrayList<>(Arrays.asList("na", "tae", "joe")));
-		game.race();
+    @Test
+    @DisplayName("최종 position을 기준으로 승자 정렬")
+    void rankFinal() {
+        //given
+        List<Car> cars = game.getCars();
+        //when
+        List<Car> result = game.rankFinal(cars);
 
-		//when
-		List<Car> result = game.rankFinal(cars);
+        assertThat(result.get(0).getPosition()).isGreaterThanOrEqualTo(result.get(1).getPosition());
+    }
 
-		assertThat(result.get(0).getPosition()).isGreaterThanOrEqualTo(result.get(1).getPosition());
-	}
+    @Test
+    @DisplayName("최종 우승자가 여러명이면 공동우승자를 선정")
+    void selectFinalWinner() {
+        //given
+        List<Car> cars = game.getCars();
+        //when
+        List<Car> winner = game.selectFinalWinner(game.rankFinal(cars));
+        long winnerNum = cars.stream().filter(m -> m.getPosition() == winner.get(0).getPosition()).count();
 
-	@Test
-	@DisplayName("최종 우승자가 여러명이면 공동우승자를 뽑는다")
-	void selectFinalWinner() {
-		//given
-		List<Car> cars = game.createCars(new ArrayList<>(Arrays.asList("na", "tae", "joe")));
-		for (int i = 0; i < 3; i++) {
-			game.race();
-		}
-		//when
-		List<Car> winner = game.selectFinalWinner(game.rankFinal(cars));
-
-		assertThat(winner.size()).isEqualTo(
-			cars.stream().filter(m -> m.getPosition() == winner.get(0).getPosition()).count());
-	}
+        assertThat(winner.size()).isEqualTo(winnerNum);
+    }
 
 }

5c1527e964f325957f079d2b160cc19f382759f9
LTSGOD <lts0424@naver.com>
2023-02-06 12:28:15 +0900

    refactor: 인터페이스 이름 변경 및 메서드 이름 변경

--- src/main/java/racingcar/util/NumGenerator.java
+++ src/main/java/racingcar/util/NumGenerator.java
@@ -1,5 +1,5 @@
 package racingcar.util;
 
-public interface Random {
-	int generateNum();
+public interface NumGenerator {
+    int getNum();
 }
--- src/main/java/racingcar/util/ZeroToNineNumGenerator.java
+++ src/main/java/racingcar/util/ZeroToNineNumGenerator.java
@@ -1,9 +1,9 @@
 package racingcar.util;
 
-public class RandomNum implements Random {
-	private static final int ZERO_TO_NINE_CONVERTER = 10;
+public class ZeroToNineNumGenerator implements NumGenerator {
+    private static final int ZERO_TO_NINE_CONVERTER = 10;
 
-	public int generateNum() {
-		return (int)(Math.random() * ZERO_TO_NINE_CONVERTER);
-	}
+    public int getNum() {
+        return (int) (Math.random() * ZERO_TO_NINE_CONVERTER);
+    }
 }

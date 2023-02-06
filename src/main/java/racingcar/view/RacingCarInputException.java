c4493b3cbbb31ace2403bce80f96bc4b40dee874
LTSGOD <lts0424@naver.com>
2023-02-06 12:29:10 +0900

    refactor: 예외처리 메서드 이름 변경

--- src/main/java/racingcar/view/RacingCarInputException.java
+++ src/main/java/racingcar/view/RacingCarInputException.java
@@ -3,54 +3,54 @@ package racingcar.view;
 import java.util.ArrayList;
 
 public class RacingCarInputException {
-	private static final int MAX_LENGTH = 5;
+    private static final int MAX_LENGTH = 5;
 
-	public static void isEmpty(String input) {
-		if (input.isEmpty()) {
-			throw new IllegalArgumentException("이름을 입력해주세요.");
-		}
-	}
+    public static void handleEmpty(String input) {
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException("이름을 입력해주세요.");
+        }
+    }
 
-	public static void isNull(String input) {
-		if (input == null) {
-			throw new IllegalArgumentException("다시 입력해주세요.");
-		}
-	}
+    public static void handleNull(String input) {
+        if (input == null) {
+            throw new IllegalArgumentException("다시 입력해주세요.");
+        }
+    }
 
-	public static void checkValidation(ArrayList<String> strings, String[] input) {
-		for (String s : input) {
-			isLessThanFiveLetters(s);
-			isDuplicatedName(strings, s);
-			isDelimiterInName(s);
-			strings.add(s);
-		}
-	}
+    public static void checkValidation(ArrayList<String> strings, String[] input) {
+        for (String s : input) {
+            handleLessThanFiveLetters(s);
+            handleDuplicatedName(strings, s);
+            handleDelimiterInName(s);
+            strings.add(s);
+        }
+    }
 
-	private static void isDelimiterInName(String s) {
-		if (s.contains(",")) {
-			throw new IllegalArgumentException("이름 사이에는 ,가 들어갈 수 없습니다.");
-		}
-	}
+    private static void handleDelimiterInName(String s) {
+        if (s.contains(",")) {
+            throw new IllegalArgumentException("이름 사이에는 ,가 들어갈 수 없습니다.");
+        }
+    }
 
-	private static void isLessThanFiveLetters(String s) {
-		if (s.length() > MAX_LENGTH) {
-			throw new IllegalArgumentException("5이하의 문자열을 입력하세요.");
-		}
-	}
+    private static void handleLessThanFiveLetters(String s) {
+        if (s.length() > MAX_LENGTH) {
+            throw new IllegalArgumentException("5이하의 문자열을 입력하세요.");
+        }
+    }
 
-	private static void isDuplicatedName(ArrayList<String> strings, String s) {
-		if (strings.contains(s)) {
-			throw new IllegalArgumentException("중복된 이름입니다. 다른이름을 입력하세요.");
-		}
-	}
+    private static void handleDuplicatedName(ArrayList<String> strings, String s) {
+        if (strings.contains(s)) {
+            throw new IllegalArgumentException("중복된 이름입니다. 다른이름을 입력하세요.");
+        }
+    }
 
-	public static void isPositive(int num) {
-		if (num < 0) {
-			throw new IllegalArgumentException("1회 이상의 수를 입력하세요.");
-		}
-	}
+    public static void handlePositive(int num) {
+        if (num < 0) {
+            throw new IllegalArgumentException("1회 이상의 수를 입력하세요.");
+        }
+    }
 
-	public static int isNum(String input) {
-		return Integer.parseInt(input);
-	}
+    public static int handleNum(String input) {
+        return Integer.parseInt(input);
+    }
 }
--- src/test/java/racingcar/view/RacingCarInputExceptionTest.java
+++ src/test/java/racingcar/view/RacingCarInputExceptionTest.java
@@ -5,65 +5,64 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class RacingCarInputExceptionTest {
 
-	@Test
-	@DisplayName("null 값 테스트")
-	void isNull() {
-		String test = null;
+    @Test
+    @DisplayName("null 값 테스트")
+    void isNull() {
+        String test = null;
 
-		Assertions.assertThrows(IllegalArgumentException.class, () -> RacingCarInputException.isNull(test));
-	}
+        assertThrows(IllegalArgumentException.class, () -> RacingCarInputException.handleNull(test));
+    }
 
-	@Test
-	@DisplayName("Empty 값 테스트")
-	void isEmpty() {
-		String test = "";
+    @Test
+    @DisplayName("Empty 값 테스트")
+    void isEmpty() {
+        String test = "";
 
-		Assertions.assertThrows(IllegalArgumentException.class, () -> RacingCarInputException.isEmpty(test));
-	}
+        assertThrows(IllegalArgumentException.class, () -> RacingCarInputException.handleEmpty(test));
+    }
 
-	@Test
-	@DisplayName("5이하의 문자열 입력 테스트")
-	void isLessThanFive() {
-		String[] test = {"na", "tae", "abcdef"};
-		ArrayList<String> result = new ArrayList<>();
+    @Test
+    @DisplayName("5이하의 문자열 입력 테스트")
+    void isLessThanFive() {
+        String[] test = {"na", "tae", "abcdef"};
+        ArrayList<String> result = new ArrayList<>();
 
-		assertAll(
-			() -> assertThrows(IllegalArgumentException.class,
-				() -> RacingCarInputException.checkValidation(result, test)),
-			() -> assertThat(result.size()).isEqualTo(2)
-		);
-	}
+        assertAll(
+                () -> assertThrows(IllegalArgumentException.class,
+                        () -> RacingCarInputException.checkValidation(result, test)),
+                () -> assertThat(result.size()).isEqualTo(2)
+        );
+    }
 
-	@Test
-	@DisplayName("중복 이름 테스트")
-	void isDuplicatedName() {
-		String[] test = {"na", "tae", "joe", "tae"};
-		ArrayList<String> result = new ArrayList<>();
+    @Test
+    @DisplayName("중복 이름 테스트")
+    void isDuplicatedName() {
+        String[] test = {"na", "tae", "joe", "tae"};
+        ArrayList<String> result = new ArrayList<>();
 
-		assertAll(
-			() -> assertThrows(IllegalArgumentException.class,
-				() -> RacingCarInputException.checkValidation(result, test)),
-			() -> assertThat(result.size()).isEqualTo(3)
-		);
-	}
+        assertAll(
+                () -> assertThrows(IllegalArgumentException.class,
+                        () -> RacingCarInputException.checkValidation(result, test)),
+                () -> assertThat(result.size()).isEqualTo(3)
+        );
+    }
 
-	@Test
-	@DisplayName("이름 사이에 , 예외 테스트")
-	void nameTest() {
-		String[] test = {"na", "ta,e", "joe"};
-		ArrayList<String> result = new ArrayList<>();
+    @Test
+    @DisplayName("이름 사이에 , 예외 테스트")
+    void nameTest() {
+        String[] test = {"na", "ta,e", "joe"};
+        ArrayList<String> result = new ArrayList<>();
 
-		assertAll(
-			() -> assertThrows(IllegalArgumentException.class,
-				() -> RacingCarInputException.checkValidation(result, test)),
-			() -> assertThat(result.size()).isEqualTo(1)
-		);
-	}
+        assertAll(
+                () -> assertThrows(IllegalArgumentException.class,
+                        () -> RacingCarInputException.checkValidation(result, test)),
+                () -> assertThat(result.size()).isEqualTo(1)
+        );
+    }
 
 }
